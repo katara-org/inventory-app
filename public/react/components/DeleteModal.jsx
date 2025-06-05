2a580ce097bfd3037f873b4bd02c52fd8f6bab56
LaneLR <laneleerichardson@gmail.com>
2025-06-05 10:17:29 -0500

    style changes to modal

--- public/react/components/DeleteModal.jsx
+++ public/react/components/DeleteModal.jsx
@@ -1,28 +1,45 @@
 import styled from "styled-components";
 
 const ModalWrapper = styled.div`
-    display: flex;
-    justify-conent: center;
-    align-items: center:
-    flex-flow: column nowrap;
-    width: 20vw;
-    max-width: 30vw;
-    min-height: 20vh;
-    max-height: 30vh;
+  display: flex; 
+  justify-content: space-evenly;
+  align-items: center;
+  flex-flow: column nowrap;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  z-index: 10;
+  padding: 1.5rem;
+  border-radius: 12px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
+`;
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 10;
 `;
 
 const BtnContainer = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-items: center;
+  width: 450px;
   height: auto;
+  margin-top: 20px;
 `;
 
 const Button = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: auto;
+  min-width: 180px;
   height: auto;
   background-color: black;
   padding: 2px 10px;
@@ -40,15 +57,18 @@ const Button = styled.div`
   }
 `;
 
-export default function DeleteModal({handleCancelClick, handleDelete, confirmDelete}) {
+export default function DeleteModal({ handleCancelClick, handleDelete }) {
   return (
     <>
-      <ModalWrapper>
-        <BtnContainer>
+      <ModalOverlay>
+        <ModalWrapper>
+            <p>Are you sure you want to delete this item?</p>
+          <BtnContainer>
             <Button onClick={handleDelete}>Confirm Delete</Button>
             <Button onClick={handleCancelClick}>Cancel</Button>
-        </BtnContainer>
-      </ModalWrapper>
+          </BtnContainer>
+        </ModalWrapper>
+      </ModalOverlay>
     </>
   );
 }
