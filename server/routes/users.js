778506f68627b56cd2d7edc38c5378aeab86cfad
LaneLR <laneleerichardson@gmail.com>
2025-06-06 10:00:47 -0500

    changed '/users/:id' for '/login'

--- server/routes/users.js
+++ server/routes/users.js
@@ -23,18 +23,18 @@ router.post('/login', async (req, res) => {
 });
 
 // POST a new user
-router.post('/login', async (req, res) => {
-  const {name, password} = req.body;
-  const user = await User.findOne({where: {name}})
-  if (!user) {
-    return res.status(404).json({error: "User doesn't exist"})
-  }
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+// router.post('/login', async (req, res) => {
+//   const {name, password} = req.body;
+//   const user = await User.findOne({where: {name}})
+//   if (!user) {
+//     return res.status(404).json({error: "User doesn't exist"})
+//   }
+//   try {
+//     const user = await User.create(req.body);
+//     res.status(201).json(user);
+//   } catch (err) {
+//     res.status(400).json({ error: err.message });
+//   }
+// });
 
 module.exports = router;
